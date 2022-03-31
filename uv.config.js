c0cd14ceeb8dfd94402c1d40eda92831ec2d835a
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-31 09:17:38 +0900

    Create uv.config.js

---
+++ uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'https://ultra-69ce5hn5i-taisei-devv.vercel.app/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
+};
